e4561a2333265358220aefe19e9c10c34f87553d
Anthony Mariotti <anthony.mariotti@yahoo.com>
2022-05-31 16:11:51 -0400

    This seems to work but needs full testing

--- src/main/java/com/anthonymariotti/toolsavior/mixins/SaviorAnvilScreenHandlerModification.java
+++ src/main/java/com/anthonymariotti/toolsavior/mixins/SaviorAnvilScreenHandlerModification.java
@@ -1,7 +1,10 @@
 package com.anthonymariotti.toolsavior.mixins;
 
 import com.anthonymariotti.toolsavior.interfaces.mixins.ISaviorAnvilModification;
+import net.minecraft.enchantment.Enchantment;
 import net.minecraft.inventory.Inventory;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.screen.AnvilScreenHandler;
 import net.minecraft.screen.Property;
 import org.slf4j.Logger;
@@ -11,7 +14,11 @@ import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import java.util.Map;
 
 @Mixin(AnvilScreenHandler.class)
 public abstract class SaviorAnvilScreenHandlerModification implements ISaviorAnvilModification {
@@ -23,19 +30,24 @@ public abstract class SaviorAnvilScreenHandlerModification implements ISaviorAnv
     @Shadow
     private int repairItemUsage;
 
+    private Boolean isEnchantment;
 
-    @Inject(method = "updateResult()V", at = @At("HEAD"))
-    public void injectUpdateResult(CallbackInfo info) {
-        TSLOGGER.info("=============================================");
-        TSLOGGER.info("Updating Result");
-        TSLOGGER.info("Level Cost: " + levelCost.get());
-        TSLOGGER.info("Repair Item Usage: " + repairItemUsage);
-        TSLOGGER.info("=============================================");
+    @Inject(
+            method = "updateResult()V",
+            at = @At(
+                    value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isOf(Lnet/minecraft/item/Item;)Z",
+                    ordinal = 0,
+                    shift = At.Shift.AFTER
+            )
+    )
+    public void isRepairing(CallbackInfo info) {
+        TSLOGGER.info("Testing");
     }
 
     @Overwrite
     public static int getNextCost(int cost) {
         TSLOGGER.info("getNextCost Called");
-        return 1;
+        // TODO: Do testing to determine if this is working as intended.
+        return 0;
     }
 }
